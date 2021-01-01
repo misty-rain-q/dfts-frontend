6d9208b5667665c852b7c565e0a630f097e7f9aa
misty-rain-q <2252107164@qq.com>
2021-01-01 22:29:05 +0800

    Achieve use ant-design-vue by used Ref #29

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 import Vue from 'vue';
-import ant from 'ant-design-vue/dist/antd';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+// import ant from 'ant-design-vue/dist/antd';
 import 'ant-design-vue/dist/antd.css';
+import Button from 'ant-design-vue/lib/button';
+import Modal from 'ant-design-vue/lib/modal';
+import Input from 'ant-design-vue/lib/input';
+import CheckBox from 'ant-design-vue/lib/checkbox';
+import Upload from 'ant-design-vue/lib/upload';
+import Select from 'ant-design-vue/lib/select';
+import InputNumber from 'ant-design-vue/lib/input-number';
+import App from './App.vue';
+import store from './store';
+import router from './router';
 
-Vue.use(ant);
+// Vue.use(ant);
+Vue.use(Button);
+Vue.use(Modal);
+Vue.use(Input);
+Vue.use(CheckBox);
+Vue.use(Upload);
+Vue.use(Select);
+Vue.use(InputNumber);
 
 Vue.config.productionTip = false;
 

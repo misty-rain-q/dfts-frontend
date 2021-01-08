a91447542b696f528cd71f06108b05f903f7d5b6
Yang Hanlin <mattoncis@hotmail.com>
2021-01-09 06:19:50 +0800

    Detect backend URL and add as GUN peer Ref #26

--- src/main.js
+++ src/main.js
@@ -9,10 +9,16 @@ import Upload from 'ant-design-vue/lib/upload';
 import Select from 'ant-design-vue/lib/select';
 import InputNumber from 'ant-design-vue/lib/input-number';
 import VueGun from 'vue-gun';
+import Gun from 'gun';
 import Progress from 'ant-design-vue/lib/progress';
+
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
 import App from './App.vue';
 import store from './store';
 import router from './router';
+
 import 'gun/lib/rindexed';
 import 'gun/lib/radix';
 import 'gun/lib/radisk';
@@ -31,22 +37,74 @@ Vue.use(Select);
 Vue.use(InputNumber);
 Vue.use(Progress);
 
+Vue.use(VueAxios, axios);
+
 Vue.config.productionTip = false;
 
 Vue.use(VueGun, {
-
   peers: [
-
+    // empty at start; first peer is added after backend base URL is detected
+    // do NOT add options here; this instance will be replaced by another in
+    // the `created` hook below
   ],
-
   // radisk: true,
-
   // localStorage: false,
-
 });
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
+  methods: {
+    async detectBackendBaseUrl() {
+      const candidates = [
+        window.location.origin,
+        `${window.location.protocol}//${window.location.hostname}:7001`,
+      ];
+      let result = null;
+      for (let i = 0; i < candidates.length; i += 1) {
+        const candidate = candidates[i];
+        try {
+          // eslint-disable-next-line no-await-in-loop
+          await axios.get(`${candidate}/api/state`);
+          result = candidate;
+          break;
+        // eslint-disable-next-line no-empty
+        } catch (error) {}
+      }
+      return result;
+    },
+  },
+  created() {
+    this.detectBackendBaseUrl()
+      .then((baseUrl) => {
+        if (baseUrl) {
+          console.log(`Backend base URL detected as ${baseUrl}`);
+
+          // Update Axios settings
+          axios.defaults.baseURL = baseUrl;
+          axios.defaults.headers.common.Authorization = 'THIS_IS_A_TOKEN'; // TODO Add a token after the backend completes authorization mechanism
+          console.log('Successfully updated Axios settings according to backend base URL');
+
+          // Add first GUN peer
+          // https://gun.eco/docs/DAM#adding-peers
+          // this.$gun.back('opt.mesh').say({
+          //   dam: 'opt',
+          //   opt: {
+          //     peers: `${baseUrl}/gun`,
+          //   },
+          // });
+          // FIXME The method above does not seem to work; re-create the gun
+          // instance as a workaround
+          Vue.prototype.$gun = Gun({
+            peers: [
+              `${baseUrl}/gun`,
+            ],
+          });
+          console.log(`Added first GUN peer ${baseUrl}/gun`);
+        } else {
+          console.error('Backend base URL cannot be detected');
+        }
+      });
+  },
 }).$mount('#app');

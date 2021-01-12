036f51beb8a23430aabda93735737e451547836a
misty-rain-q <2252107164@qq.com>
2021-01-12 21:49:32 +0800

    Complete the extraction password page initially Ref #57

--- src/components/DownloadButton.vue
+++ src/components/DownloadButton.vue
@@ -5,6 +5,12 @@
     <a-button type="primary" icon="download" @click="turnToDownloadPage">
       下载
     </a-button>
+    <a-modal ok-text="确定" cancel-text="取消"
+     :maskClosable= "false" v-model="passwordInputVisible" title="下载" @ok="passwordHandleOk">
+      <div>请输入提取码：</div>
+      <a-input id="passwordInput" placeholder="输入文件提取码"/>
+      &nbsp;
+    </a-modal>
     <a-modal ok-text="下载" cancel-text="取消"
      :maskClosable= "false" v-model="visible" title="下载" @ok="handleOk">
       <download-window></download-window>
@@ -23,17 +29,22 @@ export default {
   data() {
     return {
       visible: false,
+      passwordInputVisible: false,
     };
   },
   methods: {
-    onSearch(value) {
-      console.log(value);
+    passwordHandleOk() {
       this.visible = true;
+      this.passwordInputVisible = false;
     },
     turnToDownloadPage() {
       // const inputElement = document.getElementById('ainput');
       // console.log(inputElement.value); //该行会影响unit test
-      this.visible = true;
+      this.passwordInputVisible = true;
+    },
+    onSearch(value) {
+      console.log(value);
+      this.turnToDownloadPage();
     },
     handleOk() {
       this.visible = false;
